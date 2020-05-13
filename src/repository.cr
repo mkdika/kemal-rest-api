7d00a13b12f1c85d93ac76d75b111c36ccc4a18c
Maikel Chandika <mkdika@gmail.com>
2020-05-14 06:55:17 +0700

    Update repository to be able to use db conn string from environment variable

--- src/repository.cr
+++ src/repository.cr
@@ -3,10 +3,10 @@ require "crecto"
 module Repository
   extend Crecto::Repo
 
-  ENV["DATABASE_URL"] ||= "postgres://postgres:postgres@localhost:5432/kemal_rest_api?initial_pool_size=2&max_pool_size=5"
+  ENV["DB_URL"] ||= "postgres://postgres:postgres@localhost:5432/kemal_rest_api?initial_pool_size=2&max_pool_size=5"
 
   config do |conf|
     conf.adapter = Crecto::Adapters::Postgres
-    conf.uri = ENV["DATABASE_URL"]
+    conf.uri = ENV["DB_URL"]
   end
 end
